c71c381a2b2a07b856361911ab4e35fcc5d07076
Guilherme Babugia <babugia@Guilhermes-MacBook-Pro.local>
2019-10-01 20:27:30 -0300

    feat: added ADD_TODO

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,16 @@
+import uuidv4 from 'uuid/v4';
+
 export default function reducer(state, action) {
   const todo = action.payload;
   switch (action.type) {
+    case 'ADD_TODO':
+      const newTodo = {
+        id: uuidv4(),
+        text: action.payload,
+        complete: false
+      };
+      const todos = [...state.todos, newTodo];
+      return { ...state, todos };
     case 'TOGGLE_TODO':
       const toggledTodos = state.todos.map(t =>
         t.id === todo.id ? { ...action.payload, complete: !todo.complete } : t
