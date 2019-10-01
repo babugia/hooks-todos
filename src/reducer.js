976cf3087dfd7143332c28765ecc32e0eedc8308
Guilherme Babugia <babugia@Guilhermes-MacBook-Pro.local>
2019-10-01 20:25:23 -0300

    feat: added REMOVE_TODO

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,14 @@
 export default function reducer(state, action) {
+  const todo = action.payload;
   switch (action.type) {
     case 'TOGGLE_TODO':
-      const todo = action.payload;
       const toggledTodos = state.todos.map(t =>
         t.id === todo.id ? { ...action.payload, complete: !todo.complete } : t
       );
       return { ...state, todos: toggledTodos };
+    case 'REMOVE_TODO':
+      const filteredTodos = state.todos.filter(t => t.id !== todo.id);
+      return { ...state, todos: filteredTodos };
     default:
       return state;
   }
